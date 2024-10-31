90b70335616d6c10b43c0afe24b8308ccdd3edbc
adrian <adrian.munnoz@qrr.es>
2024-10-31 19:25:01 +0100

    First working implementation

--- roots/maize_animation/main.py
+++ roots/maize_animation/main.py
@@ -1,8 +1,19 @@
 from maize_animation import root_grow_animation
+import numpy as np
 
 if __name__ == "__main__":
     fps = 15
     total_duration = 5*60
-    growth_speed = 10 / total_duration / fps
+    growth_speed = 40.0 / total_duration / fps
     min_length_to_develop = total_duration
-    root_grow_animation.save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_speed=growth_speed, growth_steps=fps*total_duration, fps=fps, min_length_to_develop=min_length_to_develop)
+    frames = fps*total_duration
+    rps = 2.0 / 60.0
+    angular_velocity = 2*np.pi * rps / fps
+    root_grow_animation.save_maize_root_growth_animation(filename="maize_root_growth_rotation.mp4",
+                                                         growth_speed=growth_speed,
+                                                         growth_steps=frames,
+                                                         fps=fps,
+                                                         min_length_to_develop=min_length_to_develop,
+                                                         angular_velocity=angular_velocity)
+
+#mysql-connector-java-8.0.29.jar
--- roots/maize_animation/root_grow_animation.py
+++ roots/maize_animation/root_grow_animation.py
@@ -7,20 +7,32 @@ from time import sleep
 MAX_ROOTS = 1000
 MIN_LENGHT_TO_DEVELOP = 200
 
-def save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_speed=0.3, growth_steps=100, fps=15, min_length_to_develop=MIN_LENGHT_TO_DEVELOP):
+def save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_speed=0.3, growth_steps=100, fps=15,
+                                     min_length_to_develop=MIN_LENGHT_TO_DEVELOP, 
+                                     branch_probability='y', angular_velocity=0.0):
     fig, ax = plt.subplots()
     fig.set_tight_layout(True)
+    fig.patch.set_facecolor('black')
     ax.set_xlim(-10, 10)
     ax.set_ylim(-20, 0.1)
     ax.set_aspect('equal')
     ax.axis("off")
 
     # Initialize root growth parameters
-    roots = [np.array([0, 0])]
-    angles = [1.5*np.pi]
-    lines = [ax.plot([], [], color='k', lw=2)[0] for _ in range(MAX_ROOTS)]
+    roots = [np.array([0, 0, 0])]
+    angles = [(1.5*np.pi, 1.5*np.pi)]
+    bifurcation_probability = 0.07
+    max_roots = int((growth_steps - min_length_to_develop)*bifurcation_probability)
+    max_alpha = 1.0
+    min_alpha = 0.01
+    # make alphas decay faster
+    weights = np.arange(max_roots)[::-1]
+    max_weight = np.max(weights)
+    min_weight = np.min(weights)
+    alphas = (weights - min_weight) / (max_weight - min_weight) * (max_alpha - min_alpha) + min_alpha
+    lines = [ax.plot([], [], color='white', lw=2, alpha=alphas[i])[0] for i in range(max_roots)]
     angle_variation_amplitude = np.pi / 2
-    bifurcation_probability = 0.1
+    drift_proportion = 0.1
 
     # Complete branches
 
@@ -33,46 +45,75 @@ def save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_sp
         result_roots = []
         result_angles = []
         # Expand each line
-        for branch_to_expand, angle_to_follow, line in zip(roots, angles, lines):
+        for index in range(len(roots)):
+            branch_to_expand = roots[index]
+            angle_x_to_follow, angle_z_to_follow = angles[index]
+            line = lines[index]
             # For each final node, expand
             # choose number of bifurcations
-            angle_variation = np.random.uniform(-angle_variation_amplitude, angle_variation_amplitude)
-            angle = angle_to_follow + angle_variation
+            angle_variation_x = angle_variation_amplitude * np.random.uniform(-1, 1)
+            angle_variation_z = angle_variation_amplitude * np.random.uniform(-1, 1)
+            angle_x = angle_x_to_follow + angle_variation_x
+            angle_z = angle_z_to_follow + angle_variation_z
 
             # Calculate new node
-            dx = growth_speed * np.cos(angle)
-            dy = growth_speed * np.sin(angle)
+            dx = growth_speed * np.cos(angle_x) * np.cos(angle_z)
+            dy = growth_speed * max(-1, np.sin(angle_x) * np.cos(angle_z) - drift_proportion)
+            dz = growth_speed * np.cos(angle_x) * np.sin(angle_z)
 
             # Append new segment to the root
-            new_node = branch_to_expand[-1] + np.array([dx, dy])
+            new_node = branch_to_expand[-1] + np.array([dx, dy, dz])
             upper_bound = np.random.uniform(-growth_speed, 0.0)
-            new_node[1] = min(new_node[1], upper_bound)
+            if upper_bound <= new_node[1]:
+                new_node[1] = upper_bound
             # If branch for next iteration
             result_roots.append(np.vstack([branch_to_expand, new_node]))
-            result_angles.append(angle_to_follow)
+            result_angles.append((angle_x_to_follow, angle_z_to_follow))
         
         # Test to bifurcate
         if len(roots[0])> min_length_to_develop:
-            while np.random.uniform(0.0, 1.0) < bifurcation_probability and len(result_roots)<MAX_ROOTS:
-                probabilities = np.square(np.asarray([root[-1,1] for root in roots]))
+            while np.random.uniform(0.0, 1.0) < bifurcation_probability and len(result_roots)<max_roots:
+                if branch_probability == "uniform":
+                    probabilities = np.ones(len(roots))
+                elif branch_probability == "y":
+                    probabilities = np.square(np.asarray([root[-1,1] for root in roots]))
+                elif branch_probability == "L2":
+                    probabilities = np.square(np.asarray([root[-1,1]*root[-1,1] + root[-1,0]*root[-1,0] for root in roots]))
+                elif branch_probability == "length":
+                    probabilities = np.asarray([len(root) for root in roots])
+
                 probabilities = probabilities / probabilities.sum()
+                    
                 index = np.random.choice(range(len(roots)), p=probabilities)
                 branch_to_expand = roots[index]
-                lenght = np.random.randint(1, len(branch_to_expand))
-                branch_to_divide = branch_to_expand[:lenght]
-                result_roots.append(branch_to_divide)
-                result_angles.append(angle_to_follow + np.random.choice([-1, 1])*(np.pi/4 + np.random.uniform(-angle_variation_amplitude, angle_variation_amplitude)))
-
+                index_node = np.random.randint(len(branch_to_expand)//2, len(branch_to_expand))
+                result_roots.append(branch_to_expand[index_node].reshape(-1, 3))
+                random_offset_x = angle_variation_amplitude*np.random.choice([-1, 1])
+                random_noise_x = angle_variation_amplitude/2*np.random.normal(0.0, 1.0)
+                random_offset_z = angle_variation_amplitude*np.random.choice([-1, 1])
+                random_noise_z = angle_variation_amplitude/2*np.random.normal(0.0, 1.0)
+                angle_x = angle_x_to_follow + random_offset_x + random_noise_x
+                angle_z = angle_z_to_follow + random_offset_z + random_noise_z
+                result_angles.append((angle_x, angle_z))
+        # Rotate roots
+        angle_rotation = float(angular_velocity) * float(frame)
+        rotation_matrix = np.array([
+            [np.cos(angle_rotation), 0, -np.sin(angle_rotation)],
+            [0, 1, 0],
+            [np.sin(angle_rotation), 0, np.cos(angle_rotation)],
+        ])
         roots.clear()
         angles.clear()
         for root, angle in zip(result_roots, result_angles):
             roots.append(root)
             angles.append(angle)
+
         print(f"Iteration {frame}/{growth_steps}, roots {len(roots)}", end="\r")
         for line, root in zip(lines, roots):
-            x, y = root[:,0], root[:,1]
+            result_root = root @ rotation_matrix
+            x, y = result_root[:,0], result_root[:,1]
             line.set_data(x,y)
-            line.set_linewidth(0.5)
+            line.set_linewidth(1.0)
         return lines
 
     ani = animation.FuncAnimation(fig, update, frames=growth_steps, init_func=init, blit=True, repeat=False)
