4b67b3b80ffb2b3c567519893f0aa8a22791d603
adrian <adrian.munnoz@qrr.es>
2024-11-20 14:07:09 +0100

    Several fixes

--- roots/maize_animation/main.py
+++ roots/maize_animation/main.py
@@ -2,18 +2,20 @@ from maize_animation import root_grow_animation
 import numpy as np
 
 if __name__ == "__main__":
-    fps = 15
-    total_duration = 5*60
+    fps = 30
+    total_duration = 2*60
     growth_speed = 40.0 / total_duration / fps
-    min_length_to_develop = total_duration
+    min_length_to_develop = 1#total_duration
     frames = fps*total_duration
     rps = 2.0 / 60.0
     angular_velocity = 2*np.pi * rps / fps
+    np.random.seed(0)
     root_grow_animation.save_maize_root_growth_animation(filename="maize_root_growth_rotation.mp4",
                                                          growth_speed=growth_speed,
                                                          growth_steps=frames,
                                                          fps=fps,
                                                          min_length_to_develop=min_length_to_develop,
-                                                         angular_velocity=angular_velocity)
+                                                         angular_velocity=angular_velocity,
+                                                         n_trees=10)
 
 #mysql-connector-java-8.0.29.jar
--- roots/maize_animation/root_grow_animation.py
+++ roots/maize_animation/root_grow_animation.py
@@ -7,9 +7,59 @@ from time import sleep
 MAX_ROOTS = 1000
 MIN_LENGHT_TO_DEVELOP = 200
 
+def validate_dim(array, shape):
+    array_shape = array.shape
+    condition = len(array.shape) == len(shape) and all(a == b for a, b, in zip(array.shape, shape))
+    if not condition:
+        input_dim = ", ".join(str(a) for a in array_shape)
+        expected_dim = ", ".join(str(a) for a in shape)
+        message = f"input {input_dim} != Expected {expected_dim}"
+    assert condition, message
+    return condition
+
+def expand_update_branch(branch, angles, angle_variation_amplitude, growth_speed, drift_proportion):
+    """
+    branch :float[N, 3, n_trees]
+    angles :float[N, 2, n_trees]
+    """
+    n_trees = branch.shape[2]
+    node_to_expand = branch[[-1]]
+    angle_to_expand = angles[[-1]]
+    assert validate_dim(node_to_expand, [1, 3, n_trees]), angle_to_expand.shape
+    assert validate_dim(angle_to_expand, [1, 2, n_trees]), angle_to_expand.shape
+    # Compute new orientations
+    angle = angle_to_expand + angle_variation_amplitude * np.random.uniform(-1, 1, size=angle_to_expand.shape)
+    angle_x = angle[:,0]
+    angle_z = angle[:,1]
+    assert validate_dim(angle, [1, 2, n_trees])
+    assert validate_dim(angle_x, [1, n_trees])
+    assert validate_dim(angle_z, [1, n_trees])
+
+    # Calculate new node
+    dx = growth_speed * np.cos(angle_x) * np.cos(angle_z)
+    dy = growth_speed * np.maximum(-1, np.sin(angle_x) * np.cos(angle_z) - drift_proportion)
+    dz = growth_speed * np.cos(angle_x) * np.sin(angle_z)
+    assert validate_dim(dx, [1, n_trees])
+    assert validate_dim(dy, [1, n_trees])
+    assert validate_dim(dz, [1, n_trees])
+    branch_segment = np.stack((dx, dy, dz), axis=1)
+    assert validate_dim(branch_segment, [1, 3, n_trees])
+
+    # Append new segment to the root
+    new_node = node_to_expand + branch_segment
+    assert validate_dim(new_node, [1, 3, n_trees])
+    upper_bound = np.random.uniform(-growth_speed, 0.0)
+    indices_to_update = (new_node[:,1,:] >= upper_bound).ravel()
+    assert validate_dim(indices_to_update, [n_trees])
+    new_node[:,1,indices_to_update] = upper_bound
+    updated_branch = np.vstack([branch, new_node])
+    assert validate_dim(updated_branch, [len(branch)+1, 3, n_trees])
+    return updated_branch, angle
+
 def save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_speed=0.3, growth_steps=100, fps=15,
                                      min_length_to_develop=MIN_LENGHT_TO_DEVELOP, 
-                                     branch_probability='y', angular_velocity=0.0):
+                                     branch_probability='y', angular_velocity=0.0,
+                                     n_trees = 1, weight_time_delta=0.001):
     fig, ax = plt.subplots()
     fig.set_tight_layout(True)
     fig.patch.set_facecolor('black')
@@ -19,19 +69,25 @@ def save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_sp
     ax.axis("off")
 
     # Initialize root growth parameters
-    roots = [np.array([0, 0, 0])]
-    angles = [(1.5*np.pi, 1.5*np.pi)]
+    branches = [np.zeros(shape=(1, 3, n_trees))]
+    angles = [np.stack([[1.5*np.pi, 0.0] for _ in range(n_trees)]).reshape(1, 2, n_trees)]
+    weight = np.random.uniform(0.0, 1.0, n_trees)
+    weight /= weight.sum()
+    weights = [weight]
+    
+
     bifurcation_probability = 0.07
     max_roots = int((growth_steps - min_length_to_develop)*bifurcation_probability)
     max_alpha = 1.0
     min_alpha = 0.01
     # make alphas decay faster
-    weights = np.arange(max_roots)[::-1]
-    max_weight = np.max(weights)
-    min_weight = np.min(weights)
-    alphas = (weights - min_weight) / (max_weight - min_weight) * (max_alpha - min_alpha) + min_alpha
+    aplha_weights = np.arange(max_roots)[::-1]
+    max_weight = np.max(aplha_weights)
+    min_weight = np.min(aplha_weights)
+    alphas = (aplha_weights - min_weight) / (max_weight - min_weight) * (max_alpha - min_alpha) + min_alpha
     lines = [ax.plot([], [], color='white', lw=2, alpha=alphas[i])[0] for i in range(max_roots)]
-    angle_variation_amplitude = np.pi / 2
+    angle_variation_amplitude = np.pi / 16
+    angle_variation_branch = angle_variation_amplitude
     drift_proportion = 0.1
 
     # Complete branches
@@ -44,57 +100,36 @@ def save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_sp
     def update(frame):
         result_roots = []
         result_angles = []
-        # Expand each line
-        for index in range(len(roots)):
-            branch_to_expand = roots[index]
-            angle_x_to_follow, angle_z_to_follow = angles[index]
-            line = lines[index]
+        # Expand each present branch
+        for branch, angle in zip(branches, angles):
+            assert validate_dim(branch, [len(branch), 3, n_trees])
             # For each final node, expand
-            # choose number of bifurcations
-            angle_variation_x = angle_variation_amplitude * np.random.uniform(-1, 1)
-            angle_variation_z = angle_variation_amplitude * np.random.uniform(-1, 1)
-            angle_x = angle_x_to_follow + angle_variation_x
-            angle_z = angle_z_to_follow + angle_variation_z
-
-            # Calculate new node
-            dx = growth_speed * np.cos(angle_x) * np.cos(angle_z)
-            dy = growth_speed * max(-1, np.sin(angle_x) * np.cos(angle_z) - drift_proportion)
-            dz = growth_speed * np.cos(angle_x) * np.sin(angle_z)
-
-            # Append new segment to the root
-            new_node = branch_to_expand[-1] + np.array([dx, dy, dz])
-            upper_bound = np.random.uniform(-growth_speed, 0.0)
-            if upper_bound <= new_node[1]:
-                new_node[1] = upper_bound
+            updated_branch, updated_orientation = expand_update_branch(branch, angle, angle_variation_amplitude, growth_speed, drift_proportion)
+            assert validate_dim(updated_branch, [len(branch)+1, 3, n_trees])
+            assert validate_dim(updated_orientation, [1, 2, n_trees])
             # If branch for next iteration
-            result_roots.append(np.vstack([branch_to_expand, new_node]))
-            result_angles.append((angle_x_to_follow, angle_z_to_follow))
+            result_roots.append(updated_branch)
+            result_angles.append(updated_orientation)
         
         # Test to bifurcate
-        if len(roots[0])> min_length_to_develop:
+        if len(branches[0])> min_length_to_develop:
             while np.random.uniform(0.0, 1.0) < bifurcation_probability and len(result_roots)<max_roots:
-                if branch_probability == "uniform":
-                    probabilities = np.ones(len(roots))
-                elif branch_probability == "y":
-                    probabilities = np.square(np.asarray([root[-1,1] for root in roots]))
-                elif branch_probability == "L2":
-                    probabilities = np.square(np.asarray([root[-1,1]*root[-1,1] + root[-1,0]*root[-1,0] for root in roots]))
-                elif branch_probability == "length":
-                    probabilities = np.asarray([len(root) for root in roots])
-
+                probabilities = np.square(np.asarray([np.sum(-branch[-1,1]) for branch in branches]))
                 probabilities = probabilities / probabilities.sum()
                     
-                index = np.random.choice(range(len(roots)), p=probabilities)
-                branch_to_expand = roots[index]
+                index = np.random.choice(range(len(branches)), p=probabilities)
+                branch_to_expand = branches[index]
+                angle_to_expand = angles[index]
                 index_node = np.random.randint(len(branch_to_expand)//2, len(branch_to_expand))
-                result_roots.append(branch_to_expand[index_node].reshape(-1, 3))
-                random_offset_x = angle_variation_amplitude*np.random.choice([-1, 1])
-                random_noise_x = angle_variation_amplitude/2*np.random.normal(0.0, 1.0)
-                random_offset_z = angle_variation_amplitude*np.random.choice([-1, 1])
-                random_noise_z = angle_variation_amplitude/2*np.random.normal(0.0, 1.0)
-                angle_x = angle_x_to_follow + random_offset_x + random_noise_x
-                angle_z = angle_z_to_follow + random_offset_z + random_noise_z
-                result_angles.append((angle_x, angle_z))
+
+                result_roots.append(branch_to_expand[index_node].reshape(1, 3, n_trees))
+                random_offset = np.random.choice([-1, 1], size=(1, 2, n_trees))
+                noise = 0.5*np.random.normal(0.0, 1.0, size=(1, 2, n_trees))
+                new_angle = angle_to_expand + angle_variation_branch*(random_offset+noise)
+                assert validate_dim(new_angle, [1, 2, n_trees])
+                
+                result_angles.append(new_angle)
+        
         # Rotate roots
         angle_rotation = float(angular_velocity) * float(frame)
         rotation_matrix = np.array([
@@ -102,15 +137,27 @@ def save_maize_root_growth_animation(filename="maize_root_growth.mp4", growth_sp
             [0, 1, 0],
             [np.sin(angle_rotation), 0, np.cos(angle_rotation)],
         ])
-        roots.clear()
+
+        branches.clear()
         angles.clear()
-        for root, angle in zip(result_roots, result_angles):
-            roots.append(root)
+        for branch, angle in zip(result_roots, result_angles):
+            branches.append(branch)
             angles.append(angle)
 
-        print(f"Iteration {frame}/{growth_steps}, roots {len(roots)}", end="\r")
-        for line, root in zip(lines, roots):
-            result_root = root @ rotation_matrix
+        print(f"Iteration {frame}/{growth_steps}, roots {len(branches)}", end="\r")
+
+        for line, branch in zip(lines, branches):
+            assert validate_dim(branch, [len(branch), 3, n_trees])
+            # Branch of shape n_nodes, n_trees, n_dim
+            n_nodes = branch.shape[0]
+            weight_change = weight_time_delta*np.random.uniform(-1.0, 1.0, n_trees)
+            new_weights = weights[-1] + weight_change
+            new_weights /= new_weights.sum()
+            weights.append(new_weights)
+            collapsed_branch = branch @ new_weights
+            assert validate_dim(collapsed_branch, [len(collapsed_branch), 3])
+
+            result_root = collapsed_branch @ rotation_matrix
             x, y = result_root[:,0], result_root[:,1]
             line.set_data(x,y)
             line.set_linewidth(1.0)
